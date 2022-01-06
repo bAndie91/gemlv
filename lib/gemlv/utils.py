0a625cb42be7469322a28aa3edcaa0a83553e757
Andreas Hrubak <ad132454@uucp.hu>
2022-01-06 16:55:00 +0100

    add scan-participants scripts to maintain recipients-database

---
+++ lib/gemlv/utils.py
@@ -0,0 +1,19 @@
+
+import re
+import email
+
+def fix_unquoted_comma(s):
+	"""Many MUA do not enclose real names which contain comma, in quotation.
+	This causes issues in splitting address lines into individual addesses.
+	Try to work around this."""
+	def repl(m):
+		probably_realname = m.group(2)
+		probably_realname_norm = probably_realname.strip()
+		if probably_realname_norm == '' or probably_realname_norm.startswith('"') or probably_realname.find(',')==-1:
+			return m.group(0)
+		else:
+			return '%s "%s" %s' % (m.group(1), probably_realname_norm, m.group(3))  # @notranslate
+	return re.sub('(^|,)(.+)(<)', repl, s)
+
+def getaddresses(array):
+	return email.utils.getaddresses(map(fix_unquoted_comma, array))
