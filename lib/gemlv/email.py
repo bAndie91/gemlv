af169aa8be2976b8888c9237d9012c23a8962a90
Andreas Hrubak <ad132454@uucp.hu>
2022-01-24 04:04:40 +0100

    add timer and start moving NewEmail into gemlv.email.Email class

--- lib/gemlv/email.py
+++ lib/gemlv/email.py
@@ -2,6 +2,21 @@
 
 import email
 
+# TODO: factor out this block
+import time
+import traceback
+def timed(func):
+	def timed_wrap(*p, **kw):
+		t0 = time.time()
+		r = func(*p, **kw)
+		t1 = time.time()
+		delta = t1-t0
+		print "** %s %.2f" % (func.__name__, delta)
+		if delta > 0.2:
+			traceback.print_stack()
+		return r
+	return timed_wrap
+
 class Email(object):
 	def __init__(self, email_obj):
 		self.email = email_obj
@@ -9,14 +24,21 @@ class Email(object):
 	def __getitem__(self, item):
 		return self.email.__getitem__(item)
 	
+	def __setitem__(self, item, x):
+		return self.email.__setitem__(item, x)
+	
+	def __delitem__(self, item):
+		return self.email.__delitem__(item)
+	
 	def get_all(self, *args, **kwargs):
 		return self.email.get_all(*args, **kwargs)
-
+	
 	@property
 	def _headers(self):
 		return self.email._headers
 	
-	
+
+	@timed
 	def as_string(self):
 		return self.email.as_string()
 	
@@ -45,3 +67,24 @@ class Email(object):
 	
 	def get_filename(self):
 		return self.email.get_filename()
+	
+	def set_type(self, *args, **kwargs):
+		return self.email.set_type(*args, **kwargs)
+	
+	def set_param(self, *args, **kwargs):
+		return self.email.set_type(*args, **kwargs)
+	
+	@property
+	def preamble(self):
+		return self.email.preamble
+	
+	@preamble.setter
+	def preamble(self, s):
+		self.email.preamble = s
+	
+	def attach(self, *args, **kwargs):
+		return self.email.attach(*args, **kwargs)
+
+	def add_header(self, *args, **kwargs):
+		return self.email.add_header(*args, **kwargs)
+
